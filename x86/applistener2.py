a154edd23fb4b51c08e56e73e2108bc05effd868
alextaoultsides <taoofalex@yahoo.com>
2013-09-30 21:09:10 -0400

    New applistener with functional beat game.

--- x86/applistener2.py
+++ x86/applistener2.py
@@ -11,19 +11,72 @@ import glob
 import time
 from pygame.locals import *
 
-
+timer = pygame.time.Clock()
+playsound = USEREVENT + 1
+pygame.time.set_timer(playsound, 1000)
+horiz = 640
+vert = 480
+b = False
 pygame.init()
-display_surf = pygame.display.set_mode((640,480), pygame.HWSURFACE) #sets hardware surface
+display_surf = pygame.display.set_mode((horiz,vert), pygame.HWSURFACE) #sets hardware surface
 
 if not pygame.font: print 'Warning, fonts disabled'
 if not pygame.mixer: print 'Warning, sound disabled'
 bits = 16
-pygame.mixer.pre_init(44100, -bits, 2,2048)
+pygame.mixer.pre_init(44100, -bits, 2,64)
 d = {"C4","D4","E4"}
-songs = [("E4",.9),("D4",1),("C4",1),("D4",1),("E4",1),("E4",1),("E4",2),("D4",1),("D4",1),("D4",2),("E4",1),("G4",1),("G4",2),("E4",1),("D4",1),("C4",1),("D4",1),("E4",1),("E4",1),("E4",1)]
+songs = [("",1),("E4",.9),("D4",1),("C4",1),("D4",1),("E4",1),("E4",1),("E4",2),("",1),("D4",1),("D4",1),("D4",2),("",1),("E4",1),("G4",1),("G4",2),("",1),("E4",1),("D4",1),("C4",1),("D4",1),("E4",1),("E4",1),("E4",1),("",1),("D4",1),("D4",1),("E4",1),("D4",1),("C4",1),("",1),("",1)]
+
+
+
+class beatbox:
+    def __init__(self):
+        self.sounders = sounder()
+        self.i = 0
+        
+        self.beatArray = [] # stores array of pygame Rect objects
+        self.beatCount = 0  # for looping through beat array
+        self.b = False      # True or False triggered every second
+        for i in range(4): #appends Rect object to the beat array 
+            self.beatArray.append(pygame.Rect(0,0,50,50))
+        
+        #moves the boxes for collision testing
+        self.beatArray[0].center = (int(horiz *.5),int(vert * .75))
+        self.beatArray[1].center =(int(horiz*.25),int(vert *.5))
+        self.beatArray[2].center =(int(horiz*.75),int(vert *.5))
+        self.beatArray[3].center =(int(horiz *.5),int(vert *.25))
+        
+    def interaction(self,x,y):#checks for collisions and plays current note in song.
+        a = 0
+        
+        if pygame.event.get(playsound): #triggers event every second and updates beat spot and song note
+            a = pygame.time.get_ticks()
+            #winsound.Beep(440,10)
+            self.i += 1
+            self.b = True
+            if self.beatCount < 3 :
+                self.beatCount += 1
+                
+            else:
+                self.beatCount = 0
+            
+        if (self.beatArray[self.beatCount -1 ].collidepoint(x,y) and self.b == True):
+            #pygame.draw.circle(display_surf,(255,0,0),(int(640 * .5) ,int(480 * .25)), 20 + 10)
+            if songs[self.i][0] != "":
+                pygame.mixer.music.load("C:pianos/Piano_mf_"+ songs[self.i][0] +".wav")#plays note using piano sample
+                pygame.mixer.music.play()
+                
+                self.b = False   
+            
+    def check_hit(self):#point system for accuracy
+        pass
+        
+            
+                
 class sounder:
     def __init__(self):
         self.a = 0
+        self.i = 0
     def blip(self,j):
         b = 10
         if(j == 1):
@@ -36,27 +89,27 @@ class sounder:
             pygame.draw.circle(display_surf,(255,0,0),(int(640 * .75) ,int(480 * .50)), 20 + b)
         
     def songPlay(self):
+        print "hello"
         num = 1
-        for i in songs:
-            winsound.PlaySound("C:pianos/Piano.mf."+ i[0] +".wav",winsound.SND_FILENAME|winsound.SND_ASYNC)
-            if(num == 5):
-                num = 1
-            self.blip(num)
-            time.sleep(i[1])
-            num += 1
-            pygame.draw.circle(display_surf,(255,0,0),(int(640 * .5) ,int(480 * .25)), 20)
-            pygame.draw.circle(display_surf,(255,0,0),(int(640 * .5) ,int(480 * .75)), 20)
-            pygame.draw.circle(display_surf,(255,0,0),(int(640 * .25) ,int(480 * .50)), 20)
-            pygame.draw.circle(display_surf,(255,0,0),(int(640 * .75) ,int(480 * .50)), 20)
-                
+        pygame.mixer.music.load("C:pianos/Piano_mf_"+ songs[self.i][0] +".wav")
+        pygame.mixer.music.play()
+        #winsound.PlaySound("C:pianos/Piano.mf."+ songs[i][0] +".wav",winsound.SND_FILENAME|winsound.SND_ASYNC)
+        if(num == 5):
+            num = 1
+        #self.blip(num)
+        #time.sleep(i[1])
+        #num += 1
+        pygame.draw.circle(display_surf,(255,0,0),(int(640 * .5) ,int(480 * .25)), 20)
+        pygame.draw.circle(display_surf,(255,0,0),(int(640 * .5) ,int(480 * .75)), 20)
+        pygame.draw.circle(display_surf,(255,0,0),(int(640 * .25) ,int(480 * .50)), 20)
+        pygame.draw.circle(display_surf,(255,0,0),(int(640 * .75) ,int(480 * .50)), 20)
+        
     def theremin(self,freq_l):
         
         #the number of channels specified here is NOT 
         #the channels talked about here http://www.pygame.org/docs/ref/mixer.html#pygame.mixer.get_num_channels
         
         
-        #pygame.init()
-        #_display_surf = pygame.display.set_mode(size, pygame.HWSURFACE | pygame.DOUBLEBUF)
         
         
         duration = .009        # in seconds
@@ -80,7 +133,7 @@ class sounder:
         
             #grab the x-coordinate of the sine wave at a given time, while constraining the sample to what our mixer is set to with "bits"
             buf[s][0] = int(round(max_sample*math.sin(2*math.pi*freq_l*t)))        # left
-            #buf[s][1] = int(round(max_sample*0.5*math.sin(2*math.pi*freq_l*t)))    # right
+            
             
         sound = pygame.sndarray.make_sound(buf)
         #play once, then loop forever
@@ -91,7 +144,8 @@ class applistener(Leap.Listener):
     
     def on_init(self,controller):#Initializes pygame and display surface
         self.check = 0
-        
+        self.beatboxes = beatbox()
+    
         x = y = 0
         self.horiz = 640 #Display horizontal pixels
         self.vert = 480  #Display vertical pixels
@@ -101,7 +155,7 @@ class applistener(Leap.Listener):
         self.sounders = sounder()
         #self._image_surf = pygame.image.load("myimage.jpg").convert()
         print "Connected"
-        
+        timerz = 0
     def on_connect(self,controller):
         print "Connected"
         
@@ -125,9 +179,7 @@ class applistener(Leap.Listener):
             return True
     def exits(self):
         return False
-    #def on_render(self,x,y):
-    #    pygame.draw.circle(self.display_surf,(0,255,0),(x, y), 10)
-    #    pygame.display.flip()
+    
     
     def on_cleanup(self):
         if self.on_loop == True:
@@ -139,6 +191,7 @@ class applistener(Leap.Listener):
         frame = controller.frame()  
          #polls for any user input events
         
+            #print pygame.time.get_ticks()
             
         #print pygame.display.list_modes
         
@@ -166,15 +219,19 @@ class applistener(Leap.Listener):
                     
                     percentage = float(tipy) / float(self.horiz)
                     #print percentage
+                    self.beatboxes.interaction(int(tipx), int(-(tipy)))
                     
-                    freq_split = 3200.0 * -(percentage)
-                    if self.check != freq_split:
-                        self.check = freq_split
+                    
+                    #print (tipx,tipy)
+                    #freq_split = 3200.0 * -(percentage)
+                    #if self.check != freq_split:
+                    #    self.check = freq_split
                         #pygame.mixer.stop()
-                        #self.sounders.player(freq_split)
+                        #self.sounders.player(880)
                         #winsound.Beep(int(freq_split),50)
-                    
                         
+                    
+                    
                     #print 3200.0 * -(percentage)
         """
         event = pygame.event.poll()     
@@ -182,7 +239,7 @@ class applistener(Leap.Listener):
             keyname = pygame.key.name(event.key)
     
             if event.key == pygame.K_ESCAPE:
-                print "shit"
+                
                 self.running == False
         """
         for gesture in frame.gestures(): #reads in a swipe gesture
@@ -218,8 +275,8 @@ def main():
     controller = Leap.Controller()
     applistener1 = applistener()
     controller.add_listener(applistener1)
-    sounders = sounder()
-    sounders.songPlay()
+    
+    
     print "Press Enter to quit..."
    
     sys.stdin.readline()
